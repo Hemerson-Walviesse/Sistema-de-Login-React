f36ed092c0f1c685fec7c8bfd269cffd04f39885
Hemerson <hemerson.walviesse@solutionsa.com.br>
2026-04-01 16:11:21 -0300

    atualização da variavel token

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -3,7 +3,7 @@ import { createContext, useEffect, useState } from "react";
 export const AuthContext = createContext({});
 
 export const AuthProvider = ({children}) => {
-    const [user, setUSer] = useState();
+    const [user, setUser] = useState();
 
     useEffect(()=>{
         const userToken = localStorage.getItem("user_token");
@@ -14,7 +14,7 @@ export const AuthProvider = ({children}) => {
                 (user) => user.email === JSON.parse(userToken).email
             );
 
-            if (hasUser) setUSer(hasUser[0])
+            if (hasUser) setUser(hasUser[0])
         }
     },[] );
 
@@ -26,8 +26,8 @@ export const AuthProvider = ({children}) => {
         if(hasUser?.length){
             if(hasUser[0].email === email && hasUser[0].password === password){
                 const token = Math.random().toString(36).substring(2);
-                localStorage.setItem("user_token", JSON.stringify({email,password}))
-                setUSer({email,password});
+                localStorage.setItem("user_token", JSON.stringify({email,password, token}))
+                setUser({email,password});
                 return;
             }else{
                 return "E-mail ou senha incorretos"
@@ -60,7 +60,7 @@ export const AuthProvider = ({children}) => {
     }
 
     const signout = ()=>{
-        setUSer(null);
+        setUser(null);
         localStorage.removeItem("user_token");
     }
 

60c33422825e7d4192e6bd3c4de0f47b206b81f6
Hemerson <hemerson.walviesse@solutionsa.com.br>
2026-04-02 22:32:50 -0300

    correcao

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect, useState} from "react";
+import React,{ useState} from "react";
 import { Container } from "./styles";
 import { FaBars } from "react-icons/fa";
 import Sidebar from "../Sidebar";
